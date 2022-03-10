a78c3309c623f1180900573b71b1e5f4303aab3f
Catarina Duarte <designcatarinaduarte@gmail.com>
2022-03-10 00:21:21 +0000

    VB.Net

---
+++ LanguagesExercise/LanguagesExercise/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Selecione o Idioma"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ListBox1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.Color.Black
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Português", "", "", "Inglês", "", "", "Espanhol", "", "", "Italiano", "", "", "Alemão", "", "", "Francês"})
+        Me.ListBox1.Location = New System.Drawing.Point(17, 63)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(152, 256)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(188, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 19)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "...                        "
+        Me.Label2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(402, 56)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(40, 24)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(175, 90)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(267, 233)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(208, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "                      "
+        Me.Label3.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(463, 336)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ LanguagesExercise/LanguagesExercise/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Label2.Visible = True
+
+        Select Case ListBox1.SelectedItem
+            Case "Português"
+                Label2.Text = "Olá! Bem-Vindo a Portugal!"
+                PictureBox1.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\portugal.jpg")
+                PictureBox2.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\pt.jpg")
+
+            Case "Inglês"
+                Label2.Text = "Hello! Welcome to England!"
+                PictureBox1.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\unitedki.jpg")
+                PictureBox2.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\uk.jpg")
+
+            Case "Espanhol"
+                Label2.Text = "Hola! Bienvenido a Espanã!"
+                PictureBox1.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\espanha.jpg")
+                PictureBox2.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\spain.jpg")
+
+            Case "Italiano"
+                Label2.Text = "Ciao! Benvenuto in Italia!"
+                PictureBox1.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\24884919.jpg")
+                PictureBox2.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\it.jpg")
+
+
+            Case "Alemão"
+                Label2.Text = "Hallo! Willkommen in DE! "
+                PictureBox1.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\gr.jpg")
+                PictureBox2.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\alm.jpg")
+
+            Case "Francês"
+                Label2.Text = "Salut!  Bienvenue a France!"
+                PictureBox1.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\frança.jpg")
+                PictureBox2.Image = Image.FromFile("C:\Users\35196\source\repos\Tarefaidiomas\Tarefaidiomas\fr.jpg")
+
+        End Select
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
